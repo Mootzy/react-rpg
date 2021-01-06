87e42c625c15418d9516dadc5e7604c547294882
Mootzy <tyler77156@gmail.com>
2021-01-06 10:34:23 -0700

    updated css to local file

--- src/index.jsx
+++ src/index.jsx
@@ -8,3 +8,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+
+
+
+
+//this is the code replaced by './index.css' in the index.html file https://unpkg.com/purecss@2.0.3/build/pure-min.css
